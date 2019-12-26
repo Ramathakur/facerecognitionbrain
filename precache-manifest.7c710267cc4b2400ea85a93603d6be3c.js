00a1f324fef501b0ff435faf49b182553a929265
Rama Thakur <thakurc110@gmail.com>
2019-12-26 16:42:04 +0530

    Updates

---
+++ precache-manifest.7c710267cc4b2400ea85a93603d6be3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb442f87d7b326b3c1cdb819c12443d",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "7923108100cb66983762",
+    "url": "/facerecognitionbrain/static/css/2.1e00258c.chunk.css"
+  },
+  {
+    "revision": "6b38a45bcd3dcda32978",
+    "url": "/facerecognitionbrain/static/css/main.ffead349.chunk.css"
+  },
+  {
+    "revision": "7923108100cb66983762",
+    "url": "/facerecognitionbrain/static/js/2.ca039e3b.chunk.js"
+  },
+  {
+    "revision": "0da7a4f22d3c4a94734c6af8fa17fc54",
+    "url": "/facerecognitionbrain/static/js/2.ca039e3b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b38a45bcd3dcda32978",
+    "url": "/facerecognitionbrain/static/js/main.00f0a800.chunk.js"
+  },
+  {
+    "revision": "a570eb701d1e37481cb4",
+    "url": "/facerecognitionbrain/static/js/runtime-main.9b7d25f9.js"
+  }
+]);
